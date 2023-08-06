a6d3777485ebf4708d89f8348ef1674b8aabce84
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-08-06 17:20:48 +0530

    fix - added config args for cors, for allowing cookie

--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -2,10 +2,6 @@ export const sendToken = (res, user, statusCode, message) => {
   const token = user.getJWTToken();
 
   const options = {
-    sameSite: 'None',
-    domain: '.netlify.com',
-    httpOnly: true,
-    secure: true,
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
