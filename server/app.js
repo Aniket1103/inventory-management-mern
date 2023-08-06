6839ef73baac4b8257749c14cde29e9c9fedc973
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-08-06 10:24:23 +0530

    fix - added sameSite and secure attributes to cookie to solve cookie storing issue

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ import inventoryItem from './routes/inventoryItem.js'
 
 export const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
 
 app.use('/api/v1', user);
 app.use('/api/v1/inventory', inventoryItem);
--- server/utils/sendToken.js
+++ server/utils/sendToken.js
@@ -2,7 +2,9 @@ export const sendToken = (res, user, statusCode, message) => {
   const token = user.getJWTToken();
 
   const options = {
+    sameSite: 'None',
     httpOnly: true,
+    secure: true,
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
