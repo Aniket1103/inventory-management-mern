7cb224acfda6fdb0e340bb32f0182485661ff4e1
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-08-07 20:15:00 +0530

    fix - added PATCH method to cors config, to solve cors issue

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
 }));
 
