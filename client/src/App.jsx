006226601c2a357d8803801a05d4ca72968418a4
Aniket1103 <55594740+Aniket1103@users.noreply.github.com>
2023-08-07 07:25:34 +0530

    feat - implemented getting current user login for starting the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 // import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
 import Login from './components/Login/Login';
@@ -6,7 +6,23 @@ import Register from './components/Register/Register';
 import Dashboard from './components/Dashboard/Dashboard'; 
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(0);
+  const [currentUser, setCurrentUser] = useState(null);
+
+  const getUser = async () => {
+    try {
+      const { data } = await axios.get(`https://inventory-management-quhz.onrender.com/api/v1/currentUser`, {
+        withCredentials: true
+      })
+      console.log("Current User: ", data);
+      setCurrentUser(data);
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  }
+  useEffect(() => {
+    console.log(import.meta.env)
+    getUser();
+  }, [])
 
   return (
     <Router>
